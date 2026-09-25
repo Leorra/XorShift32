09b456bddbb713c5e28e64bcd87a0a355d413e37
Leorra <alex_lisn@hotmail.com>
2026-09-24 21:06:10 -0400

    Initial commit

--- XorShift32.hpp
+++ XorShift32.hpp
@@ -1,12 +1,12 @@
-﻿// =======================================================================
+﻿// ====================================================================
 // [+] Lock-free thread-safe XorShift32 bit random number generator [+] 
 // https://github.com/Leorra/
-// =======================================================================
-
+// ====================================================================
 #pragma once
 
 #include <atomic>
 #include <cstdint>
+#include <limits>
 
 class XorShift32 {
 private:
@@ -14,6 +14,21 @@ private:
 	std::atomic<std::uint32_t> rng_state_ { kDefaultSeed_ };
 
 public:
+	// URBG Interface Requirements
+	using result_type = std::uint32_t;
+
+	[[nodiscard]] static constexpr result_type min() noexcept {
+		return std::numeric_limits<result_type>::min();
+	}
+
+	[[nodiscard]] static constexpr result_type max() noexcept {
+		return std::numeric_limits<result_type>::max();
+	}
+
+	result_type operator()() noexcept {
+		return xorShift32();
+	}
+
 	explicit XorShift32(std::uint32_t seed = kDefaultSeed_) noexcept { setSeed(seed); }
 
 	void setSeed(std::uint32_t seed) noexcept {
@@ -25,8 +40,7 @@ public:
 		std::uint32_t current = rng_state_.load(std::memory_order_relaxed);
 		std::uint32_t next = 0;
 
-		do { std::uint32_t x = current; x ^= x << 13; x ^= x >> 17; x ^= x << 5; next = x; }
-		while (!rng_state_.compare_exchange_weak(
+		do { std::uint32_t x = current; x ^= x << 13; x ^= x >> 17; x ^= x << 5; next = x; } while (!rng_state_.compare_exchange_weak(
 			current, next, std::memory_order_relaxed, std::memory_order_relaxed
 		)); return next;
 	}
@@ -50,7 +64,7 @@ public:
 		if (n <= 1) [[unlikely]] { return 0; }
 		std::uint64_t multi = static_cast<std::uint64_t>(xorShift32()) * n;
 		std::uint32_t low = static_cast<std::uint32_t>(multi);
-		if (low < n) { 
+		if (low < n) {
 			const std::uint32_t threshold = (0U - n) % n;
 			while (low < threshold) {
 				multi = static_cast<std::uint64_t>(xorShift32()) * n;
